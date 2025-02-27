a47cf5900cd587b019354570d6c9acfa584e9bd8
Jonathan <jonathan.js.schledt@bmw.de>
2025-02-27 15:03:37 +0100

    Phonebook backend step 8

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(morgan("tiny"));
-
-morgan.token("type", (req, res) => {
-	return req.headers["content-type"];
+morgan.token("body", (req) => {
+	return JSON.stringify(req.body);
 });
+
+app.use(
+	morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
 	{
