d401459632e6f4ec75615e3153b440bbaee38662
Jonathan <jonathan.js.schledt@bmw.de>
2025-02-27 12:58:05 +0100

    Phonebook backend step 6

--- index.js
+++ index.js
@@ -55,6 +55,43 @@ app.delete("/api/persons/:id", (request, response) => {
 	response.status(204).end();
 });
 
+const generateId = () => {
+	const Id = Math.floor(Math.random() * 10000000);
+	return String(Id);
+};
+
+app.post("/api/persons", (request, response) => {
+	const body = request.body;
+
+	if (!body.name) {
+		return response.status(400).json({
+			error: "name is missing",
+		});
+	}
+
+	if (!body.number) {
+		return response.status(400).json({
+			error: "number is missing",
+		});
+	}
+
+	if (persons.find((person) => person.name === request.body.name)) {
+		return response.status(400).json({
+			error: "name must be unique",
+		});
+	}
+
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: generateId(),
+	};
+
+	persons = persons.concat(person);
+
+	response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
